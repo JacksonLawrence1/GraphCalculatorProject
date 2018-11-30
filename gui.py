7101c1912cc338417f2af87a7bacd05ddb1a85c5
Jackson Lawrence <42301022+JacksonLawrence1@users.noreply.github.com>
2018-11-30 09:54:19 +0000

    Allows individual inputs
    allows you to chose what type of graph you want in the console

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 # creates the screen with axis
 import equations
 import pygame
-from itertools import cycle
+import equation_inputs
 
 # Initialises the pygame module
 pygame.init()
@@ -12,7 +12,7 @@ y_axis = (-10, 10)
 graph_axis = equations.Axis(x_axis[0], x_axis[1], y_axis[0], y_axis[1])
 
 height = 720
-width = 1280
+width = 720
 screen_size = (width, height)  # Sets screen size
 screen = pygame.display.set_mode(screen_size)
 pygame.display.set_caption("Graph Calculator")  # Title of game at the top
@@ -29,7 +29,24 @@ y_axis_difference = y_axis[1] - y_axis[0]
 x_factor = width / x_axis_difference
 y_factor = height / y_axis_difference
 
-equation_coordinates = equations.Polynomial(graph_axis).points
+equation_input = input("Enter what type of graph you want (Polynomial, Circle, Trigonometric or Log) >>> ").upper()
+repeat_input = True
+while repeat_input:
+    if equation_input == "POLYNOMIAL":
+        equation_coordinates = equations.Polynomial(graph_axis, equation_inputs.polynomial_input()).points
+        repeat_input = False
+    elif equation_input == "CIRCLE":
+        equation_coordinates = equations.Circle(graph_axis, equation_inputs.circle_centre_input(), equation_inputs.circle_radius_input()).points
+        repeat_input = False
+    elif equation_input == "TRIGONOMETRIC" or "TRIG":
+        equation_coordinates = equations.Trigonometric(graph_axis, equation_inputs.trigonometric_function_input()).points
+        repeat_input = False
+    elif equation_input == "LOG":
+        equation_coordinates = equations.Logarithm(graph_axis, equation_inputs.log_inputs()).points
+        repeat_input = False
+    else:
+        equation_input = input("Enter a suitable graph (Polynomial, Circle, Trigonometric or Log) >>>")
+
 for item in equation_coordinates:
     item[0] = (item[0] + (x_axis_difference / 2)) * x_factor
     item[1] = abs(item[1] - (y_axis_difference / 2)) * y_factor
